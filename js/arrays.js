11a3bf5025755b6351f9d52343af2d387051e287
Facundo Grondona <facundogrondona02@gmail.com>
2025-04-27 13:42:08 -0300

    FEAT(ALL): Cree el primer modelo de la app, ya capture los datos de la api y estan las funcionalidades basicas y carrito

---
+++ js/arrays.js
@@ -0,0 +1,52 @@
+
+function filtrarProductosPorNombre() {
+    let valor = prompt('Ingrese el nombre de producto a filtrar:')
+    valor = valor.trim().toLowerCase()
+    
+    // Camino Feliz
+    // let productosEncontrados = productos.filter((producto)=> producto.nombre === valor )
+
+    // Búsqueda dentro de un texto
+    // let productosEncontrados = productos.filter((producto)=> producto.nombre.includes(valor) )
+
+    // Normalizar texto de búsqueda
+    let productosEncontrados = productos.filter((producto)=> producto.nombre.toLowerCase().includes(valor) )
+    console.log(productosEncontrados)
+}
+
+/*
+    {
+        id: "1",
+        imagen: "🍌",
+        nombre: "Bananas",
+        precio: 1220,
+        categoria: "Fruta"
+    }
+*/
+
+function mapearProductos() {
+    let nuevoArray = productos.map((producto)=> {
+        return {
+            codigo: producto.id,
+            nombre: producto.nombre.toUpperCase(),
+            importe: producto.precio
+        }
+    })
+    console.log(nuevoArray)
+}
+
+function ordenarProductos() {
+    let veces = 0
+    productos.sort((a, b)=> { // ordenar por precio
+        veces++
+        if (a.precio > b.precio) {
+            return -1
+        }
+        if (a.precio < b.precio) {
+            return 1
+        }
+        return 0
+    })
+    console.log("Veces iteradas:", veces)
+    console.table(productos)
+}
